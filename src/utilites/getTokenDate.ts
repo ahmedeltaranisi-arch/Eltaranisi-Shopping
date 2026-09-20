02eb39311db24d03b5a2268023013057a2cbc0c4
ahmedeltaranisi-arch <ahmedeltaranisi@example.com>
2026-09-20 23:36:51 +0300

    fix cart token on vercel

--- src/utilites/getTokenDate.ts
+++ src/utilites/getTokenDate.ts
@@ -1,15 +1,17 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function getTokenFun(){
- 
- const cookie = await cookies();
-  const nextAuthToken = cookie.get("next-auth.session-token")?.value;
+export async function getTokenFun() {
+  const cookieStore = await cookies();
+  const nextAuthToken =
+    cookieStore.get("__Secure-next-auth.session-token")?.value ||
+    cookieStore.get("next-auth.session-token")?.value;
+
+  if (!nextAuthToken) return null;
+
   const accessToken = await decode({
     secret: process.env.NEXTAUTH_SECRET!,
     token: nextAuthToken,
   });
- return accessToken?.token;
-
-
+  return accessToken?.token;
 }
